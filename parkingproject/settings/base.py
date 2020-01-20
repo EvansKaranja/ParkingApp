1dc7b0c81185fc828fd2e8260f03c0f06f565866
EvansKaranja <evanskndungu@gmail.com>
2020-01-20 19:14:33 +0300

    added parking app

--- parkingproject/settings/base.py
+++ parkingproject/settings/base.py
@@ -34,7 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis'
+    'django.contrib.gis',
+     'rest_framework',
+     'parking'
 ]
 
 MIDDLEWARE = [
