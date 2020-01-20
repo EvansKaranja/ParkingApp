c2a5007d2f9fc90f0f130421d934fab1cd7fcd77
EvansKaranja <evanskndungu@gmail.com>
2020-01-20 19:22:35 +0300

    commented rest_framework

--- parkingproject/settings/base.py
+++ parkingproject/settings/base.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-     'rest_framework',
+    #  'rest_framework',
      'parking'
 ]
 
