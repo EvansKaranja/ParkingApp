80bca32fece0fc6d39c9f5a308237a463b1bea29
EvansKaranja <evanskndungu@gmail.com>
2020-01-22 16:59:12 +0300

    changed the map to white theme

--- parkingproject/settings/base.py
+++ parkingproject/settings/base.py
@@ -124,6 +124,6 @@ STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
