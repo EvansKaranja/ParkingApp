d6e1e41932c5bddf82cd59b7c7abbbad3a1bdc9f
EvansKaranja <evanskndungu@gmail.com>
2020-02-04 19:25:29 +0300

    changed the permission

--- parking/views.py
+++ parking/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.generics import CreateAPIView
-
+from rest_framework.permissions import AllowAny
 from .serializers import MpesaSerializer, MpesaTransaction
 
 
 class PaymentList(CreateAPIView):
     queryset = MpesaTransaction.objects.all()
     serializer_class = MpesaSerializer
+    permission_classes = [AllowAny]
 
     def create(self, request):
         print('Receiving Data')
