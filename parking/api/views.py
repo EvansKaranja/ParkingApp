3b867c344a9fd2b0e66bf0952c1966650ed02658
EvansKaranja <evanskndungu@gmail.com>
2020-03-14 20:46:18 +0300

    defined more models

--- parking/api/views.py
+++ parking/api/views.py
@@ -4,36 +4,27 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import MpesaSerializer
 from parking.models import MpesaPayments
-from parking.mpesa import lipanampesa
+# from parking.mpesa import lipanampesa
 from rest_framework.permissions import AllowAny
 
-# {
-#     "phoneNumber": "254728547196",
-#     "amount": 200
-# }
+
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def make_payments(request):
     if request.method == 'POST':
-        # print(request.data)
         vehicleType = request.data["vehicleType"]
-        print("received")
         vehicleReg = request.data["vehicleReg"]
         duration = request.data["duration"]
         mobileNumber = request.data["mobileNumber"]
-        print(mobileNumber)
         amount = request.data["amount"]
-        print(amount)
         parkingspace = request.data["parkingspace"]
         request_type = request.data["type"]
         if request_type == "pay":
             res = lipanampesa.lipa_na_mpesa(phone=mobileNumber, amount=amount)
             return Response({"payed": False})
         else:
-            print("sleeping for 6osec")
             time.sleep(60)
             res = lipanampesa.lipa_na_mpesa(phone=mobileNumber, amount=amount)
-            print("waited for 1 minute")
             return Response({"payed": False})
     return Response({"message": "Hello, world!"})
 
@@ -44,34 +35,24 @@ def LNMtransact(request):
     print(mpesaTransactions)
     # serializer = MpesaSerializer(mpesaTransactions)
     if request.method == 'POST':
-        print(request.data)
         Merchant_request_id = request.data["Body"]["stkCallback"]["MerchantRequestID"]
-        print("Merchant_request_id=", Merchant_request_id)
         Checkout_request_id = request.data["Body"]["stkCallback"]["CheckoutRequestID"]
-        print("Checkout_request_id=", Checkout_request_id)
         Result_code = request.data["Body"]["stkCallback"]["ResultCode"]
-        print("Result_code=", Result_code)
         Result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
-        print("Result_description=", Result_description)
         if Result_code == 0:
             Amount = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][0]["Value"]
-            print("Amount=", Amount)
             Mpesa_receipt_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][1]["Value"]
-            print("Mpesa_receipt_number=", Mpesa_receipt_number)
-            Transaction_date = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][3]["Value"]
-            str_transaction_date = str(Transaction_date)
-            formatted_transaction_datetime = datetime.strptime(
-                str_transaction_date, "%Y%m%d%H%M%S")
-            print("Transaction_date=",  formatted_transaction_datetime)
+            Transaction_date = str(
+                request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][3]["Value"])
+            Transaction_datetime = datetime.strptime(
+                Transaction_date, "%Y%m%d%H%M%S")
             Phone_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][4]["Value"]
-            print("Phone_number=", Phone_number)
-
             transaction = MpesaPayments.objects.create(
                 MerchantRequestID=Merchant_request_id,
                 CheckoutRequestID=Checkout_request_id,
                 Amount=Amount,
                 MpesaReceiptNumber=Mpesa_receipt_number,
-                TransactionDate=formatted_transaction_datetime,
+                TransactionDate=Transaction_datetime,
                 PhoneNumber=Phone_number)
             transaction.save()
             return Response({"payed": True})
--- parkingproject/settings/development.py
+++ parkingproject/settings/development.py
@@ -21,3 +21,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+        'rest_framework.permissions.AllowAny'
+    ]
+}
