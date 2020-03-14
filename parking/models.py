4139fb30cc65413b05bc56c42b0ca76a5d1562a3
EvansKaranja <evanskndungu@gmail.com>
2020-03-14 21:37:51 +0300

    fixing migrations

--- parking/models.py
+++ parking/models.py
@@ -3,40 +3,40 @@ from django.contrib.gis.db import models
 # Create your models here.
 
 
-# class ParkingSpaces(models.Model):
-#     parking_space_id = models.AutoField(primary_key=True)
-#     parking_type = models.CharField(max_length=100, null=True)
-#     owner = models.CharField(max_length=20, null=True)
-#     streetName = models.CharField(max_length=100, null=True)
-#     active = models.BooleanField(default=False, null=True)
-#     zone = models.CharField(max_length=50, null=True)
-#     lat = models.FloatField(null=True)
-#     lat = models.FloatField(null=True)
+class ParkingSpaces(models.Model):
+    parking_space_id = models.AutoField(primary_key=True)
+    parking_type = models.CharField(max_length=100, null=True)
+    owner = models.CharField(max_length=20, null=True)
+    streetName = models.CharField(max_length=100, null=True)
+    active = models.BooleanField(default=False, null=True)
+    zone = models.CharField(max_length=50, null=True)
+    lat = models.FloatField(null=True)
+    lat = models.FloatField(null=True)
 
-#     centroid = models.PointField(null=True)
+    centroid = models.PointField(null=True)
 
-#     class Meta:
-#         verbose_name_plural = 'Parking Spaces'
+    class Meta:
+        verbose_name_plural = 'Parking Spaces'
 
-#     def __str__(self):
-#         return f"{self.owner}"
+    def __str__(self):
+        return f"{self.owner}"
 
 
-# class ParkingDetails(models.Model):
-#     parkingSpace = models.ForeignKey(
-#         'ParkingSpaces', on_delete=models.CASCADE, related_name="parkingspace")
-#     duration = models.DurationField()
-#     vehicle_type = models.CharField(max_length=20)
-#     vehicle_registration_number = models.CharField(max_length=10)
-#     time_of_parking = models.DateTimeField(auto_now_add=True)
-#     payments_details = models.ForeignKey(
-#         'MpesaPayments', on_delete=models.CASCADE, related_name="payment")
+class ParkingDetails(models.Model):
+    parkingSpace = models.ForeignKey(
+        'ParkingSpaces', on_delete=models.CASCADE, related_name="parkingspace")
+    duration = models.DurationField()
+    vehicle_type = models.CharField(max_length=20)
+    vehicle_registration_number = models.CharField(max_length=10)
+    time_of_parking = models.DateTimeField(auto_now_add=True)
+    payments_details = models.ForeignKey(
+        'MpesaPayments', on_delete=models.CASCADE, related_name="payment")
 
-#     class Meta:
-#         verbose_name_plural = 'Parking Details'
+    class Meta:
+        verbose_name_plural = 'Parking Details'
 
-#     def __str__(self):
-#         return f"{self.vehicle_registration_number}"
+    def __str__(self):
+        return f"{self.vehicle_registration_number}"
 
 
 class MpesaPayments(models.Model):
